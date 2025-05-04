d2ed14b69ef9f6974a06d384970f473e4224fff1
Serkanmngck <119745696+Serkanmngck@users.noreply.github.com>
2025-05-04 15:03:47 +0300

    Admin düzeyinde ajax ile ekleme silme güncelleme işlemleri eklendi - google chart ile görselleştirme işlemı gerçekleştirildi

---
+++ WebApplication1/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,37 @@
+﻿using DotNetCoreCamp.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication1.Areas.Admin.Models;
+
+namespace DotNetCoreCamp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            list.Add(new CategoryClass
+            {
+                categoryname = "Teknoloji",
+                categorycount = 14
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Yazılım",
+                categorycount = 5
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Spor",
+                categorycount = 2
+            });
+            return Json(new { jsonlist = list });
+        }
+    }
+}
--- WebApplication1/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ WebApplication1/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -2,6 +2,8 @@
 using DataAccessLayer.Concrate;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
 
 namespace WebApplication1.Areas.Admin.ViewComponents.Statistic
 {
@@ -14,6 +16,15 @@ namespace WebApplication1.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1= blogManager.GetList().Count;
             ViewBag.v2 = context.Contacts.Count();
             ViewBag.v3 = context.Comments.Count();
+            string api = "1f24ccca50e5d6b0153e97b54fc73c43";
+            string city = "Istanbul";
+            string connection=" https://api.openweathermap.org/data/2.5/weather?q="+ city + "&mode=xml&appid=" +api;
+            XDocument  document = XDocument.Load(connection);
+            string Ktemperature = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;  // "288.55" string olarak döner
+            float kelvin = float.Parse(Ktemperature, System.Globalization.CultureInfo.InvariantCulture);
+            float celsius = kelvin - 273.15f;
+            string Ctemperature = celsius.ToString("0.0", System.Globalization.CultureInfo.InvariantCulture) + "°C";
+            ViewBag.v4 = Ctemperature;
             return View();
         }
     }
